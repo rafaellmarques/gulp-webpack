e269b05de77ab8836088141f2f84251f45d5eafd
Rafael Lourenço Marques <rafael.marques@rakuten.com.br>
2019-08-25 12:23:52 -0300

    First commit;

---
+++ tasks/scripts.js
@@ -0,0 +1,24 @@
+﻿// Packages
+const gulp = require("gulp");
+const webpack = require("webpack");
+const webpackstream = require("webpack-stream");
+const webpackconfig = require("../webpack.config");
+
+// Paths
+const paths = {
+	input: "./src/assets/js/bundle.js",
+	output: "./dist/js/"
+};
+
+// Task
+scriptTranspilation = () => {
+	return gulp
+		.src(paths.input)
+		.pipe(webpackstream(webpackconfig, webpack))
+		.pipe(gulp.dest(paths.output));
+};
+
+// Export
+module.exports = {
+	build: scriptTranspilation
+};
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+﻿const path = require("path");
+
+module.exports = {
+	mode: "none",
+	output: {
+		filename: "bundle.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js?$/,
+				include: [path.resolve(__dirname, "./src/assets/js")],
+				loader: "babel-loader"
+			}
+		]
+	}
+};
