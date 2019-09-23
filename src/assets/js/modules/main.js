f101bbfa954b96e85aed1573061a604d371c02b0
Rafael Lourenço Marques <rafaellmarques@outlook.com>
2019-09-22 23:42:25 -0300

    Correção no arquivo main.js.

--- src/assets/js/modules/main.js
+++ src/assets/js/modules/main.js
@@ -145,7 +145,6 @@ const main = {
 	},
 
 	instagram() {
-		const token = "267545657.62f8a58.b42b65c64ea44256aa50a490a6e5bb7c";
 		const link = "https://api.instagram.com/v1/users/self/media/recent/?access_token=";
 		const token = "267545657.62f8a58.b42b65c64ea44256aa50a490a6e5bb7c";
 		const instagramList = document.querySelector(".instagram-list");
